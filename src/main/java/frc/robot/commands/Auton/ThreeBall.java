2d71c3c5d9e0c94dcee8007f12e48897d9b6745e
GCS-Robotics-Driver <robotics@geneseechristianschool.com>
2022-03-26 10:19:12 -0400

    Create initial 3 ball auton

---
+++ src/main/java/frc/robot/commands/Auton/ThreeBall.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Auton;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.GroupCommands.*;
+import frc.robot.commands.IndexSub.*;
+import frc.robot.commands.IntakeSub.*;
+import frc.robot.commands.ShooterSub.TurnShooterDegrees;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IndexSub;
+import frc.robot.subsystems.IntakeSub;
+import frc.robot.subsystems.ShooterSub;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ThreeBall extends SequentialCommandGroup {
+  /** Creates a new ThreeBall. */
+  public ThreeBall(DriveSubsystem drivetrain, IndexSub index, ShooterSub shooter, IntakeSub intake) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    double driveSpeed = 0.40;
+    double waitTime = 0.2;
+    addCommands(
+      // Backup
+        new DriveDistance(driveSpeed, 50, drivetrain).withTimeout(1.5),
+      // Shoot preloaded Ball
+        new ShootCargo(index, shooter).withTimeout(1.5),
+      // Turn and Get Ball 2
+        new ParallelCommandGroup(
+            new TurnDegreesGyro(0.5, 90, drivetrain),
+            new TurnShooterDegrees(shooter, 0)),
+        new ExtendIntake(intake).withTimeout(0.2),
+        new ParallelCommandGroup(
+            new IntakeForward(intake),
+            new IndexForward(index),
+            new DriveDistance(driveSpeed, -12, drivetrain).andThen(new WaitCommand(waitTime))).withTimeout(1),
+        new IndexReverse(index).withTimeout(0.2),
+        new ShootCargo(index, shooter).withTimeout(1.5),
+      // Turn and Get Ball 3
+        new ParallelCommandGroup(
+            new RetractIntake(intake),
+            new TurnShooterDegrees(shooter, 0),
+            new TurnDegreesGyro(0.5, -90, drivetrain)),
+        new DriveDistance(driveSpeed, -40, drivetrain).withTimeout(1.5),
+        new ExtendIntake(intake).withTimeout(0.2),
+        new ParallelCommandGroup(
+            new IntakeForward(intake),
+            new IndexForward(index),
+            new DriveDistance(driveSpeed, 20, drivetrain).andThen(new WaitCommand(waitTime))).withTimeout(1),
+        new IndexReverse(index).withTimeout(0.2),
+        new ShootCargo(index, shooter).withTimeout(1.5),
+        new StopAll(shooter, index, intake, drivetrain)
+    );
+  }
+}
